92bcac4387b39c6dec5dfeabf43a896a2f0e0319
cavaevinci <ivanevacicweb+private@gmail.com>
2024-08-10 22:24:21 +0200

    added search

--- Components/LogView/LogViewController.swift
+++ Components/LogView/LogViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class LogViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class LogViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    let searchBar = UISearchBar()
+    var filteredLogMessages: [String] = []
     var logMessages: [String]
     let tableView = UITableView()
 
@@ -31,9 +33,27 @@ class LogViewController: UIViewController, UITableViewDataSource, UITableViewDel
         tableView.rowHeight = UITableView.automaticDimension
         tableView.register(LogTableViewCell.self, forCellReuseIdentifier: "LogCell")
         view.addSubview(tableView)
+        view.addSubview(searchBar)
+
+        searchBar.delegate = self
+        searchBar.placeholder = "Search logs"
+        navigationItem.titleView = searchBar
+        
+        filteredLogMessages = logMessages
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+
+        }
         
         tableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
         }
     }
     
@@ -46,13 +66,34 @@ class LogViewController: UIViewController, UITableViewDataSource, UITableViewDel
     // MARK: - UITableViewDataSource & UITableViewDelegate
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return logMessages.count
+        return filteredLogMessages.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LogCell", for: indexPath) as! LogTableViewCell
-        cell.logLabel.text = logMessages[indexPath.row]
+        cell.logLabel.text = filteredLogMessages[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - UISearchBarDelegate
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredLogMessages = logMessages
+        } else {
+            filteredLogMessages = logMessages.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+
+        tableView.reloadData()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Dismiss the keyboard when the search button is clicked
+        searchBar.resignFirstResponder()
     }
 
 }
